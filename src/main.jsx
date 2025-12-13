40c2bfe277cf848205d8523eeba32edd05797177
Manattee Vilairat <manattee2998@gmail.com>
2025-12-13 21:46:36 +0700

    edit main.jsx to show HelloWorld instead of show App.jsx Component

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
   </StrictMode>,
 )
