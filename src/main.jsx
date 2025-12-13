19063967265e90b6d86de060f7018f29fa12e116
Manattee Vilairat <manattee2998@gmail.com>
2025-12-13 21:56:33 +0700

    Update Rendered Element

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  </StrictMode>,
-)
+const showTime = () => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    </StrictMode>,
+);
+}
+
+setInterval(showTime, 1000);
