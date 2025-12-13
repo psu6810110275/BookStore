f0249d78bc0d705437ba000573d2c26a389088b7
Manattee Vilairat <manattee2998@gmail.com>
2025-12-13 22:43:05 +0700

    announce component

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BookList from './components/BookList'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const bookData = [
+{ id: 1, title: "Harry Potter", author: "J.K. Rowling", description: "Orphan Harry learns he is a wizard",
+price: 15.70, isbn: "978-1408825945", stock: 10 },
+{ id: 2, title: "Sapiens", author: "Yuval Noah Harari", description: "A brief history of humankind", price:
+22.99, isbn: "978-0062316097", stock: 50 },
+]
+
   return (
-   <table border="1">
-      <tr>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th>ISBN</th>
-        <th>Stock</th>
-      </tr>
-      <tr>
-        <td>Harry Potter</td>
-        <td>J.K. Rowling</td>
-        <td>Orphan Harry learns he is a wizard</td>
-        <td>15.70</td>
-        <td>978-1408825945</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>Sapiens</td>
-        <td>Yuval Noah Harari</td>
-        <td>A brief history of humankind</td>
-        <td>22.99</td>
-        <td>978-0062316097</td>
-        <td>50</td>
-      </tr>
-    </table>
+  <>
+    <h3>Book List</h3>
+    <BookList data={bookData}/>
+  </>
 );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const showTime = () => {
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    </StrictMode>,
-);
-}
-
-setInterval(showTime, 1000);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
