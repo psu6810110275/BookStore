6d655e64d1220fe02e5fb9298b3542fab5ef9ad3
Manattee Vilairat <manattee2998@gmail.com>
2025-12-13 22:20:34 +0700

    testing App.jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,35 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <table border="1">
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>ISBN</th>
+        <th>Stock</th>
+      </tr>
+      <tr>
+        <td>Harry Potter</td>
+        <td>J.K. Rowling</td>
+        <td>Orphan Harry learns he is a wizard</td>
+        <td>15.70</td>
+        <td>978-1408825945</td>
+        <td>10</td>
+      </tr>
+      <tr>
+        <td>Sapiens</td>
+        <td>Yuval Noah Harari</td>
+        <td>A brief history of humankind</td>
+        <td>22.99</td>
+        <td>978-0062316097</td>
+        <td>50</td>
+      </tr>
+    </table>
+);
 }
 
 export default App
